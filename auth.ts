bbb047e310aef79a674860738e70007df0d6e9f3
Jeff Claybrook <jeffclaybrook@Jeffs-MBP.home>
2024-02-02 16:35:46 -0600

    Initial commit

---
+++ auth.ts
@@ -0,0 +1,72 @@
+import { NextApiRequest, NextApiResponse, GetServerSidePropsContext } from "next"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth, { getServerSession, type NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import prisma from "@/lib/prisma"
+
+export const config = {
+ pages: {
+  signIn: "/login"
+ },
+ adapter: PrismaAdapter(prisma),
+ providers: [
+  GoogleProvider({
+   clientId: process.env.GOOGLE_CLIENT_ID!,
+   clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+  })
+ ],
+ session: {
+  strategy: "jwt"
+ },
+ callbacks: {
+  async session({ session, token }) {
+   if (token) {
+    session.user.id = token.id
+    session.user.name = token.name
+    session.user.email = token.email
+    session.user.image = token.picture
+    session.user.username = token.username
+   }
+   return session
+  },
+  async jwt({ token, user }) {
+   const prismaUser = await prisma.user.findFirst({
+    where: {
+     email: token.email
+    }
+   })
+   if (!prismaUser) {
+    token.id = user.id
+    return token
+   }
+   if (!prismaUser.username) {
+    await prisma.user.update({
+     where: {
+      id: prismaUser.id
+     },
+     data: {
+      username: prismaUser.name?.split(" ").join("").toLowerCase()
+     }
+    })
+   }
+   return {
+    id: prismaUser.id,
+    name: prismaUser.name,
+    email: prismaUser.email,
+    username: prismaUser.username,
+    picture: prismaUser.image
+   }
+  }
+ }
+} satisfies NextAuthOptions
+
+export default NextAuth(config)
+
+export function auth(
+ ...args:
+  | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+  | [NextApiRequest, NextApiResponse]
+  | []
+) {
+ return getServerSession(...args, config)
+}
---
+++ lib/utils.ts
@@ -0,0 +1,16 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+import { auth } from "@/auth"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const getUserId = async () => {
+  const session = await auth()
+  const userId = session?.user?.id
+  if (!userId) {
+    throw new Error("You must be logged in to use this feature")
+  }
+  return userId
+}
